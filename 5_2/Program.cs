75decd4a130dcfa3b8f50c4b8e3061359b61142e
Avenir Baryshev <avenirpik@gmail.com>
2025-03-22 18:36:40 +0700

    upd 2 3

--- 5_2/Program.cs
+++ 5_2/Program.cs
@@ -7,34 +7,19 @@
             const int n = 10;
             int[] arr = new int[n];
             Random rnd = new Random();
+            int minus = 0;
+            int plus = 0;
+            int zero = 0;
 
-            //Отдельно создаём и выводим массив
             for (int i = 0; i < n; i++)
             {
-                arr[i] = rnd.Next(0, 51);
+                arr[i] = rnd.Next(-20, 21);
                 Console.Write("{0} ", arr[i]);
+                if (arr[i] == 0) zero++;
+                else if (arr[i] < 0) minus++;
+                else plus++;
             }
-            int max = arr[0];
-            int max_ind = 0;
-            int min = arr[0];
-            int min_ind = 0;
-            //Обрабатываем отдельно
-            for (int i = 0; i < n; i++)
-            {
-                if (arr[i] < min)
-                {
-                    min = arr[i];
-                    min_ind = i;
-                }
-                if (arr[i] > max)
-                {
-                    max = arr[i];
-                    max_ind = i;
-                }
-            }
-            Console.WriteLine("\n\nМаксимальное = {0} с индексом {1}", max, max_ind);
-            Console.WriteLine("\nМинимальное = {0} с индексом {1}", min, min_ind);
-            Console.WriteLine("\n\nPress any key to continue");
+            Console.WriteLine("\n{0} положительных элементов\n{1} отрицательных элементов\n{2} равных нулю элементов", plus, minus, zero);
             Console.ReadKey();
         }
     }
