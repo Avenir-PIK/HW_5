f39bc92a885831db00191ddcd335a0a47c83ef60
Avenir Baryshev <avenirpik@gmail.com>
2025-03-22 17:41:46 +0700

    1 - rdy

--- 5_1/Program.cs
+++ 5_1/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nНажмите любую клавишу для выхода");
+            Console.WriteLine("Сформировать одномерный массив из 10 случайных чисел из диапазона [0, 100]. Определить, каких чисел больше – четных или нечетных");
+
+            const int n = 10;
+            int[] pachka = new int[n];
+
+            Random rnd = new Random();
+            int chet = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                pachka[i] = rnd.Next(0, 101);
+                if (pachka[i] % 2 == 0) chet++;
+                Console.Write("{0} ", pachka[i]);
+            }
+
+            Console.WriteLine("\nчётных чисел тут {0}", chet);
+            if (chet == 5) Console.WriteLine("\nчётных и нечётных одинаково", chet);
+            if (chet < 5) Console.WriteLine("\nчётных меньше", chet);
+            else Console.WriteLine("\nчётных больше", chet);
+
+            Console.WriteLine("\nPress any key to continue");
             Console.ReadKey();
         }
     }
